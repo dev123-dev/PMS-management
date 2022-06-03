50848c4a951ea4fb898c1da7cce62a0a4ea7c854
dev123-dev <dev@pinnaclemedia.in>
2022-06-03 16:50:16 +0530

    laz

--- routes/api/client.js
+++ routes/api/client.js
@@ -4,7 +4,7 @@ const config = require("config");
 const { check, validationResult } = require("express-validator");
 const mongoose = require("mongoose");
 const ClientDetails = require("../../models/Client");
-
+//ADD
 router.post("/add-client", async (req, res) => {
   let data = req.body;
   try {
@@ -17,4 +17,50 @@ router.post("/add-client", async (req, res) => {
   }
 });
 
+router.post("/edit-client", async (req, res) => {
+  try {
+    let data = req.body;
+
+    const updateEmployeeDetails = await EmployeeDetails.updateOne(
+      { _id: data.recordId },
+      {
+        $set: {
+          empFullName: data.empFullName,
+          empDepartmentId: data.empDepartmentId,
+          empDesignationId: data.empDesignationId,
+          empDesignationDate: data.empDesignationDate,
+          empJoiningDate: data.empJoiningDate,
+          empDOB: data.empDOB,
+          empAadharNo: data.empAadharNo,
+          empPanNo: data.empPanNo,
+          empPhone: data.empPhone,
+          empEmail: data.empEmail,
+          empAddress: data.empAddress,
+          empState: data.empState,
+          empPincode: data.empPincode,
+          empGroupId: data.empGroupId,
+          empStatus: data.empStatus,
+          empColorCode: data.empColorCode,
+          empEnteredById: data.empEnteredById,
+          empDate: data.empDate,
+          empDateTime: Date.now(),
+          empBankName: data.empBankName,
+          empAccountNo: data.empAccountNo,
+          empBankBranch: data.empBankBranch,
+          empIFSCCode: data.empIFSCCode,
+          empPFNo: data.empPFNo,
+          empPFDate: data.empPFDate,
+          empUANNo: data.empUANNo,
+          empESICNo: data.empESICNo,
+          empBasic: data.empBasic,
+          empHRA: data.empHRA,
+          empCA: data.empCA,
+        },
+      }
+    );
+    res.json(updateEmployeeDetails);
+  } catch (error) {
+    res.status(500).json({ errors: [{ msg: "Server Error" }] });
+  }
+});
 module.exports = router;
