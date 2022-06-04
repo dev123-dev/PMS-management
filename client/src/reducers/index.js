3dd3ad0fb88adf66853d46eded50744a04b0ed0c
lathesh <dev@pinnaclemedia.in>
2022-06-04 17:26:49 +0530

    abhi

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
-import tenants from "./tenants";
 export default combineReducers({
   alert,
   auth,
-  tenants,
 });
