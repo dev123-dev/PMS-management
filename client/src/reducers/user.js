372f06e812a75236ef89ccfcddda556fd7a42a73
lathesh <dev@pinnaclemedia.in>
2022-06-06 10:53:45 +0530

    abhi

---
+++ client/src/reducers/user.js
@@ -0,0 +1,35 @@
+import { ALL_EMPLOYEE, USER_GROUPS } from "../actions/types";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  loading: true,
+  users: [],
+  errorResponse: "",
+  successResponse: "",
+  savedMessage: "",
+  allEmployee: "",
+  userGroup: "",
+};
+
+const user = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case ALL_EMPLOYEE:
+      return {
+        ...state,
+        allEmployee: payload,
+      };
+    case USER_GROUPS:
+      return {
+        ...state,
+        userGroup: payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default user;
