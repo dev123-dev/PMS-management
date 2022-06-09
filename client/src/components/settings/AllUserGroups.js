8731953f078434ac4182648ec86b8eaee3c781a4
lathesh <dev@pinnaclemedia.in>
2022-06-09 09:59:07 +0530

    pra

--- client/src/components/settings/AllUserGroups.js
+++ client/src/components/settings/AllUserGroups.js
@@ -90,23 +90,13 @@ const AllUserGroups = ({
                               </td>
 
                               <td>
-                                <>
-                                  <img
-                                    className="img_icon_size log"
-                                    onClick={() => onUpdate(userGroups, idx)}
-                                    src={require("../../static/images/delete.png")}
-                                    alt="Deactivate"
-                                    title="Deactivate"
-                                  />
-                                  &nbsp;
-                                  <img
-                                    className="img_icon_size log"
-                                    onClick={() => onUpdate(userGroups, idx)}
-                                    src={require("../../static/images/edit_icon.png")}
-                                    alt="Edit"
-                                    title="Edit"
-                                  />
-                                </>
+                                <img
+                                  className="img_icon_size log"
+                                  onClick={() => onUpdate(userGroups, idx)}
+                                  src={require("../../static/images/edit_icon.png")}
+                                  alt="Edit"
+                                  title="Edit"
+                                />
                               </td>
                             </tr>
                           );
