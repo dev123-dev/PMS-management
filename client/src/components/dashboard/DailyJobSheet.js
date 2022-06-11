0097cfb3b43e419383201f657820ace77ff58322
dev123-dev <dev@pinnaclemedia.in>
2022-06-11 17:48:39 +0530

    pra

--- client/src/components/dashboard/DailyJobSheet.js
+++ client/src/components/dashboard/DailyJobSheet.js
@@ -1,19 +1,134 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Select from "react-select";
+import ChangeProjectLifeCycle from "./ChangeProjectLifeCycle";
 import Spinner from "../layout/Spinner";
-import { getDailyJobsheetProjectDeatils } from "../../actions/projects";
+import EditProject from "./EditProject";
+import {
+  getJobQueueProjectDeatils,
+  getAllProjectStatus,
+} from "../../actions/projects";
 
 const DailyJobSheet = ({
   auth: { isAuthenticated, user, users },
-  project: { dailyJobsheetProjects },
-  getDailyJobsheetProjectDeatils,
+  project: { jobQueueProjects, allProjectStatus },
+  getJobQueueProjectDeatils,
+  getAllProjectStatus,
 }) => {
   useEffect(() => {
-    getDailyJobsheetProjectDeatils();
-  }, [getDailyJobsheetProjectDeatils]);
-  // console.log("dailyJobsheetProjects", dailyJobsheetProjects);
+    getJobQueueProjectDeatils();
+  }, [getJobQueueProjectDeatils]);
+  useEffect(() => {
+    getAllProjectStatus();
+  }, [getAllProjectStatus]);
+
+  // On change ProjectCycle
+  const [showProjectCycleModal, setShowProjectCycleModal] = useState(false);
+  const handleProjectCycleModalClose = () => setShowProjectCycleModal(false);
+
+  const onProjectCycleModalChange = (e) => {
+    if (e) {
+      handleProjectCycleModalClose();
+    }
+  };
+
+  // Modal
+  const projectStatusOpt = [];
+  allProjectStatus.map((projStatusData) =>
+    projectStatusOpt.push({
+      label: projStatusData.projectStatusType,
+      value: projStatusData._id,
+    })
+  );
+
+  const [statusChangeValue, setStatusChange] = useState();
+  const onSliderChange = (jobQueueProjects) => (e) => {
+    // console.log("id", id);
+    // console.log("e", e);
+
+    let newStatusData = {
+      statusId: e.value,
+      value: e.label,
+      projectId: jobQueueProjects._id,
+    };
+
+    setStatusChange(newStatusData);
+    // console.log("statusChangeValue", statusChangeValue);
+    setShowProjectCycleModal(true);
+  };
+
+  const onRadioProjCatTypeChange = (e) => {
+    // console.log(e.target.value);
+    // if (e.target.value === "student") {
+    //   setFormData({ ...formData, userRole: e.target.value });
+    // } else {
+    //   setFormData({ ...formData, userRole: e.target.value });
+    // }
+  };
+  let projectQty = 0,
+    downloadingQty = 0,
+    WorkingQty = 0,
+    PendingQty = 0,
+    QCPendingQty = 0,
+    QCEstimateQty = 0,
+    UploadingQty = 0;
+
+  const [showEditModal, setShowEditModal] = useState(false);
+  const handleEditModalClose = () => setShowEditModal(false);
+
+  const onClickReset = () => {
+    getJobQueueProjectDeatils("");
+  };
+
+  const onEditModalChange = (e) => {
+    if (e) {
+      handleEditModalClose();
+    }
+  };
+  const [userDatas, setUserDatas] = useState(null);
+  const onUpdate = (jobQueueProjects, idx) => {
+    setShowEditModal(true);
+    setUserDatas(jobQueueProjects);
+  };
+  const [formData, setFormData] = useState({
+    radioselect: "",
+    isSubmitted: false,
+  });
+
+  const { radioselect } = formData;
+  const onstatuscategrorySelect = (statuscategrory) => {
+    if (statuscategrory === "Normal") {
+      setFormData({
+        ...formData,
+        radioselect: "Normal",
+      });
+    } else if (statuscategrory === "Amendment") {
+      setFormData({
+        ...formData,
+        radioselect: "Amendment",
+      });
+    } else if (statuscategrory === "Additional Instruction") {
+      setFormData({
+        ...formData,
+        radioselect: "Additional Instruction",
+      });
+    } else if (statuscategrory === "Don't Work") {
+      setFormData({
+        ...formData,
+        radioselect: "Don't Work",
+      });
+    } else {
+      setFormData({
+        ...formData,
+        radioselect: "",
+      });
+    }
+  };
+  console.log(radioselect);
+
   return !isAuthenticated || !user || !users ? (
     <Spinner />
   ) : (
@@ -21,12 +136,37 @@ const DailyJobSheet = ({
       <div className="container container_align ">
         <section className="sub_reg">
           <div className="row col-lg-12 col-md-12 col-sm-12 col-12 no_padding">
-            <div className=" col-lg-5 col-md-11 col-sm-10 col-10">
+            <div className=" col-lg-3 col-md-11 col-sm-10 col-10">
               <h5 className="heading_color">Daily Job Sheet</h5>
             </div>
-            <div className="col-lg-7 col-md-11 col-sm-12 col-11 py-3"></div>
+            <div className=" col-lg-2 col-md-11 col-sm-10 col-10">
+              <input
+                type="date"
+                placeholder="dd/mm/yyyy"
+                className="form-control cpp-input datevalidation"
+                // name="projectDate"
+                // value={startprojectDate}
+                // onChange={(e) => onDateChange(e)}
+                style={{
+                  width: "75%",
+                }}
+                required
+              />
+            </div>
+
+            <div className="col-lg-7 col-md-11 col-sm-12 col-11 py-3">
+              <button
+                className="btn btn_green_bg float-right"
+                onClick={() => onClickReset()}
+              >
+                Refresh
+              </button>
+
+              <Link className="btn btn_green_bg float-right" to="/add-Project">
+                Add
+              </Link>
+            </div>
           </div>
-          <div className="row col-lg-12 col-md-11 col-sm-12 col-12 no_padding"></div>
           <div className="row">
             <div className="col-lg-12 col-md-12 col-sm-12 col-12 text-center">
               <section className="body">
@@ -49,56 +189,63 @@ const DailyJobSheet = ({
                         <th>Status</th>
                         <th>Latest Change</th>
                         <th>Job Notes</th>
-
                         <th>OP</th>
                       </tr>
                     </thead>
                     <tbody>
-                      {dailyJobsheetProjects &&
-                        dailyJobsheetProjects.map(
-                          (dailyJobsheetProjects, idx) => {
-                            return (
-                              <tr key={idx}>
-                                <td>{dailyJobsheetProjects.clientName}</td>
-                                <td>
-                                  {dailyJobsheetProjects.clientFolderName}
-                                </td>
-                                <td>{dailyJobsheetProjects.projectName}</td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td>{dailyJobsheetProjects.projectPriority}</td>
-                                <td>{dailyJobsheetProjects.projectDeadline}</td>
-                                <td>{dailyJobsheetProjects.projectQuantity}</td>
-                                <td>
-                                  {dailyJobsheetProjects.projectStatusType}
-                                </td>
-                                <td></td>
-                                <td>{dailyJobsheetProjects.projectNotes}</td>
-                                <td></td>
-                                {/* <td>
-                                <>
-                                  <img
-                                    className="img_icon_size log"
-                                    onClick={() => onUpdate(allDepartment, idx)}
-                                    src={require("../../static/images/delete.png")}
-                                    alt="Deactivate"
-                                    title="Deactivate"
-                                  />
-                                  &nbsp;
-                                  <img
-                                    className="img_icon_size log"
-                                    onClick={() => onUpdate(allDepartment, idx)}
-                                    src={require("../../static/images/edit_icon.png")}
-                                    alt="Edit"
-                                    title="Edit"
-                                  />
-                                </>
-                              </td> */}
-                              </tr>
-                            );
-                          }
-                        )}
+                      {jobQueueProjects &&
+                        jobQueueProjects.map((jobQueueProjects, idx) => {
+                          projectQty += jobQueueProjects.projectQuantity;
+                          let statusType = jobQueueProjects.projectStatusType;
+                          if (statusType === "Downloading") downloadingQty += 1;
+                          if (statusType === "Working") WorkingQty += 1;
+                          if (statusType === "Pending") PendingQty += 1;
+                          if (statusType === "QC Pending") QCPendingQty += 1;
+                          if (statusType === "QC Estimate") QCEstimateQty += 1;
+                          if (statusType === "Uploading") UploadingQty += 1;
+
+                          return (
+                            <tr key={idx}>
+                              <td>{jobQueueProjects.clientName}</td>
+                              <td>{jobQueueProjects.clientFolderName}</td>
+                              <td>{jobQueueProjects.projectName}</td>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                              <td>{jobQueueProjects.projectPriority}</td>
+                              <td>{jobQueueProjects.projectDeadline}</td>
+                              <td>{jobQueueProjects.projectQuantity}</td>
+                              {/* <td>{jobQueueProjects.projectStatusType}</td> */}
+                              <td>
+                                <Select
+                                  name="projectStatusData"
+                                  value={{
+                                    label: jobQueueProjects.projectStatusType,
+                                    value: jobQueueProjects.projectStatusId,
+                                  }}
+                                  options={projectStatusOpt}
+                                  isSearchable={false}
+                                  placeholder="Select"
+                                  onChange={onSliderChange(jobQueueProjects)}
+                                />
+                              </td>
+                              <td></td>
+                              <td>{jobQueueProjects.projectNotes}</td>
+                              {/* <td></td> */}
+                              <td>
+                                <img
+                                  className="img_icon_size log"
+                                  onClick={() =>
+                                    onUpdate(jobQueueProjects, idx)
+                                  }
+                                  src={require("../../static/images/edit_icon.png")}
+                                  alt="Edit"
+                                  title="Edit"
+                                />
+                              </td>
+                            </tr>
+                          );
+                        })}
                     </tbody>
                   </table>
                 </div>
@@ -106,7 +253,81 @@ const DailyJobSheet = ({
             </div>
           </div>
         </section>
+
+        <div className="row col-md-12 col-lg-12 col-sm-12 col-12  bottmAlgmnt">
+          <div className="col-lg-10 col-md-6 col-sm-6 col-12">
+            <label>Downloading:{downloadingQty} &emsp;</label>
+            <label>Working : {WorkingQty}&emsp;</label>
+            <label>Pending : {PendingQty}&emsp;</label>
+            <label>QC Pending: {QCPendingQty}&emsp;</label>
+            <label>QC Estimate : {QCEstimateQty}&emsp;</label>
+            <label>Uploading: {UploadingQty}&emsp;</label>
+          </div>
+          <div className="col-lg-2 col-md-6 col-sm-6 col-12 align_right">
+            Quantity:{projectQty}
+          </div>
+        </div>
+
+        <Modal
+          show={showProjectCycleModal}
+          backdrop="static"
+          keyboard={false}
+          size="md"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header>
+            <div className="col-lg-10">
+              <h3 className="modal-title text-center">Project Life Cycle</h3>
+            </div>
+            <div className="col-lg-2">
+              <button onClick={handleProjectCycleModalClose} className="close">
+                <img
+                  src={require("../../static/images/close.png")}
+                  alt="X"
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </button>
+            </div>
+          </Modal.Header>
+          <Modal.Body>
+            <ChangeProjectLifeCycle
+              onProjectCycleModalChange={onProjectCycleModalChange}
+              ProjectCycledata={statusChangeValue}
+            />
+          </Modal.Body>
+        </Modal>
       </div>
+
+      <Modal
+        show={showEditModal}
+        backdrop="static"
+        keyboard={false}
+        size="xl"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header>
+          <div className="col-lg-10">
+            <h3 className="modal-title text-center">Edit Project Details</h3>
+          </div>
+          <div className="col-lg-2">
+            <button onClick={handleEditModalClose} className="close">
+              <img
+                src={require("../../static/images/close.png")}
+                alt="X"
+                style={{ height: "20px", width: "20px" }}
+              />
+            </button>
+          </div>
+        </Modal.Header>
+        <Modal.Body>
+          <EditProject
+            onEditModalChange={onEditModalChange}
+            allProjectdata={userDatas}
+          />
+        </Modal.Body>
+      </Modal>
     </Fragment>
   );
 };
@@ -114,12 +335,14 @@ const DailyJobSheet = ({
 DailyJobSheet.propTypes = {
   auth: PropTypes.object.isRequired,
   project: PropTypes.object.isRequired,
+  getJobQueueProjectDeatils: PropTypes.object.isRequired,
 };
 const mapStateToProps = (state) => ({
   auth: state.auth,
   project: state.project,
 });
 
-export default connect(mapStateToProps, { getDailyJobsheetProjectDeatils })(
-  DailyJobSheet
-);
+export default connect(mapStateToProps, {
+  getJobQueueProjectDeatils,
+  getAllProjectStatus,
+})(DailyJobSheet);
