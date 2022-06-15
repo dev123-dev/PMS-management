c7255b994e69172aa5a4e94f0690007c8f85e97f
dev123-dev <dev@pinnaclemedia.in>
2022-06-15 09:38:48 +0530

    laz

--- client/src/components/dashboard/JobQueue.js
+++ client/src/components/dashboard/JobQueue.js
@@ -25,6 +25,20 @@ const JobQueue = ({
     getAllProjectStatus();
   }, [getAllProjectStatus]);
 
+  function dhm(pDate, pTime) {
+    let pStartDate = new Date(pDate + "," + pTime);
+    let pEndDate = new Date();
+    let ms = Math.abs(pStartDate - pEndDate);
+    const days = Math.floor(ms / (24 * 60 * 60 * 1000));
+    const daysms = ms % (24 * 60 * 60 * 1000);
+    const hours = Math.floor(daysms / (60 * 60 * 1000));
+    const hoursms = ms % (60 * 60 * 1000);
+    const minutes = Math.floor(hoursms / (60 * 1000));
+    const minutesms = ms % (60 * 1000);
+    const sec = Math.floor(minutesms / 1000);
+    return days + " d : " + hours + " h : " + minutes + " m : " + sec + " s";
+  }
+
   // On change ProjectCycle
   const [showProjectCycleModal, setShowProjectCycleModal] = useState(false);
   const handleProjectCycleModalClose = () => setShowProjectCycleModal(false);
@@ -193,7 +207,12 @@ const JobQueue = ({
                               <td>{jobQueueProjects.clientName}</td>
                               <td>{jobQueueProjects.clientFolderName}</td>
                               <td>{jobQueueProjects.projectName}</td>
-                              <td></td>
+                              <td>
+                                {dhm(
+                                  jobQueueProjects.projectDate,
+                                  jobQueueProjects.projectTime
+                                )}
+                              </td>
                               <td></td>
                               <td></td>
                               <td>{jobQueueProjects.projectPriority}</td>
