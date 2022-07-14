b7981030e36a1a5f1caf900833654d43a31812e6
dev123-dev <dev@pinnaclemedia.in>
2022-07-14 11:46:51 +0530

    pra

--- client/src/components/dashboard/Amendments.js
+++ client/src/components/dashboard/Amendments.js
@@ -2,6 +2,8 @@ import React, { Fragment, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import Select from "react-select";
+import { Link } from "react-router-dom";
 import {
   getAllchanges,
   getAmendmentProjectDeatils,
@@ -17,11 +19,84 @@ const Amendments = ({
   //formData
   const [formData, setFormData] = useState({
     Notes: "",
+    projectStatusCategory: "",
+    discussionPoints: "",
+    UnResolved: "",
+
+    Resolved: "",
     isSubmitted: false,
   });
-  const { Notes, isSubmitted } = formData;
+
+  const StatusCategory = [
+    { value: "Resolved", label: "Resolved" },
+    { value: "UnResolved", label: "UnResolved" },
+  ];
+  const {
+    Notes,
+    projectStatusCategory,
+    discussionPoints,
+    UnResolved,
+    Resolved,
+    isSubmitted,
+  } = formData;
   const onInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onStatuscatChange = (e) => {
+    if (e) {
+      setFormData({
+        ...formData,
+        projectStatusCategory: e,
+      });
+    }
+  };
+
+  const [ProjRestore, setProjRestore] = useState(null);
+  const onClickHandler = (amendmentProjects, idx) => {
+    setShowHide({
+      ...showHide,
+      showChequenoSection: true,
+    });
+    setProjRestore(amendmentProjects);
+  };
+  const [showHide, setShowHide] = useState({
+    showChequenoSection: false,
+  });
+  const { showChequenoSection } = showHide;
+  const onRadioSelect = (radiodata) => {
+    if (radiodata === "Resolved") {
+      setFormData({
+        ...formData,
+        Resolved: "Resolved",
+        major: "",
+      });
+    } else if (radiodata === "UnResolved") {
+      setFormData({
+        ...formData,
+        Resolved: "",
+        UnResolved: "UnResolved",
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [radiodata]: 1,
+      });
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const finalData = {
+      recordId: ProjRestore ? ProjRestore._id : "",
+      projectName: ProjRestore.projectName,
+      discussionPoints: discussionPoints,
+      UnResolved: UnResolved,
+      Resolved: Resolved,
+    };
+    console.log(finalData);
+    // restoreProjectData(finalData);
+    // onRestoreModalChange(true);
   };
   return !isAuthenticated || !user || !users ? (
     <Spinner />
@@ -32,6 +107,26 @@ const Amendments = ({
           <div className="row col-lg-12 col-md-12 col-sm-12 col-12 no_padding">
             <div className=" col-lg-1 col-md-11 col-sm-10 col-10">
               <h5 className="heading_color">Amendments</h5>
+            </div>
+            <div className=" col-lg-2 col-md-11 col-sm-10 col-10 py-2">
+              <Select
+                name="projectStatusCategory"
+                options={StatusCategory}
+                isSearchable={true}
+                value={projectStatusCategory}
+                placeholder="Select"
+                onChange={(e) => onStatuscatChange(e)}
+                theme={(theme) => ({
+                  ...theme,
+                  height: 26,
+                  minHeight: 26,
+                  borderRadius: 1,
+                  colors: {
+                    ...theme.colors,
+                    primary: "black",
+                  },
+                })}
+              />
             </div>
           </div>
           <div className="row col-lg-12 col-md-12 col-sm-12 col-12 no_padding">
@@ -47,13 +142,10 @@ const Amendments = ({
                         <th style={{ width: "10%" }}>Client Name</th>
                         <th style={{ width: "6%" }}>Folder </th>
                         <th style={{ width: "25%" }}>Project Name</th>
-                        <th style={{ width: "12%" }}>Queue Duration</th>
-                        <th style={{ width: "10%" }}>Estimated Time</th>
-                        <th style={{ width: "10%" }}>Job Time</th>
-                        <th style={{ width: "2%" }}>Deadline</th>
-                        <th style={{ width: "3%" }}>Qty</th>
+                        {/* <th style={{ width: "2%" }}>Deadline</th> */}
+                        {/* <th style={{ width: "3%" }}>Qty</th> */}
                         <th style={{ width: "13%" }}>Status</th>
-                        <th style={{ width: "10%" }}>OP</th>
+                        {/* <th style={{ width: "10%" }}>OP</th> */}
                       </tr>
                     </thead>
                     <tbody>
@@ -61,26 +153,30 @@ const Amendments = ({
                         amendmentProjects.map((amendmentProjects, idx) => {
                           return (
                             <tr key={idx}>
-                              <td>{amendmentProjects.clientName}</td>
-
+                              <td>
+                                <Link
+                                  className="float-left ml-3"
+                                  to="#"
+                                  onClick={() =>
+                                    onClickHandler(amendmentProjects, idx)
+                                  }
+                                >
+                                  {amendmentProjects.clientName}
+                                </Link>
+                              </td>
                               <td>
                                 <b>{amendmentProjects.clientFolderName}</b>
                               </td>
                               <td>{amendmentProjects.projectName}</td>
-                              <td></td>
-                              <td></td>
-                              <td></td>
-
-                              <td>{amendmentProjects.projectDeadline}</td>
-                              <td>
+                              {/* <td>{amendmentProjects.projectDeadline}</td> */}
+                              {/* <td>
                                 {amendmentProjects.projectQuantity}&nbsp;
                                 {amendmentProjects.projectUnconfirmed ===
                                   true && (
                                   <span style={{ color: "red" }}>*</span>
                                 )}
-                              </td>
-                              <td></td>
-                              <td></td>
+                              </td> */}
+                              <td>{amendmentProjects.projectStatusType}</td>
                             </tr>
                           );
                         })}
@@ -91,35 +187,74 @@ const Amendments = ({
             </div>
             <div className="col-lg-4 col-md-12 col-sm-12 col-12  ">
               <div className="col-lg-12 col-md-6 col-sm-6 col-12 card-new py-2">
-                <div className="row col-lg-12 col-md-6 col-sm-6 col-12 ">
-                  <div className=" col-lg-12 col-md-6 col-sm-6 col-12 ">
-                    <label className="label-control">Discusion Points :</label>
-                    <textarea
-                      name="Notes"
-                      id="Notes"
-                      className="textarea form-control"
-                      rows="4"
-                      placeholder="Notes"
-                      style={{ width: "100%" }}
-                      value={Notes}
-                      onChange={(e) => onInputChange(e)}
-                      required
-                    ></textarea>
+                <form onSubmit={(e) => onSubmit(e)}>
+                  <div className="row col-lg-12 col-md-6 col-sm-6 col-12 ">
+                    <div className="col-lg-4 col-md-6 col-sm-6 col-12">
+                      <label className="label-control">Resolved : </label>
+                      &emsp;
+                      <input
+                        className="radiolevels"
+                        type="radio"
+                        id="Resolved"
+                        value="Resolved"
+                        name="radiolevels"
+                        onClick={() => onRadioSelect("Resolved")}
+                      />
+                    </div>
+                    <div className="col-lg-4 col-md-6 col-sm-6 col-12">
+                      <label className="label-control">Un-Resolved : </label>
+                      &emsp;
+                      <input
+                        className="radiolevels"
+                        type="radio"
+                        id="UnResolved"
+                        value="UnResolved"
+                        onClick={() => onRadioSelect("UnResolved")}
+                        name="radiolevels"
+                      />
+                    </div>
+
+                    <div className=" col-lg-12 col-md-6 col-sm-6 col-12 ">
+                      <label className="label-control">
+                        Discussion Points :
+                      </label>
+                      <textarea
+                        name="discussionPoints"
+                        id="discussionPoints"
+                        className="textarea form-control"
+                        rows="4"
+                        placeholder="discussionPoints"
+                        style={{ width: "100%" }}
+                        value={discussionPoints}
+                        onChange={(e) => onInputChange(e)}
+                        required
+                      ></textarea>
+                    </div>
+                    {showChequenoSection && (
+                      <div className="col-lg-12 col-md-6 col-sm-6 col-12 ">
+                        <input
+                          type="submit"
+                          name="Submit"
+                          value="Submit"
+                          className="btn sub_form btn_continue blackbrd Save float-right"
+                        />
+                      </div>
+                    )}
                   </div>
-                  <div className="col-lg-12 col-md-6 col-sm-6 col-12 ">
-                    <input
-                      type="submit"
-                      name="Submit"
-                      value="Submit"
-                      className="btn sub_form btn_continue blackbrd Save float-right"
-                    />
-                  </div>
-                </div>
+                </form>
               </div>
 
-              <div className="col-lg-12 col-md-6 col-sm-6 col-12 card-new py-2">
+              <div className="row col-lg-12 col-md-6 col-sm-6 col-12 card-new py-2">
                 <div className="col-lg-12 col-md-6 col-sm-6 col-12 ">
-                  <label className="label-control">Last Discusion :</label>
+                  <Link
+                    className="btn btn_green_bg float-right"
+                    // to="/add-client"
+                  >
+                    History
+                  </Link>
+                </div>
+                <div className="col-lg-12 col-md-6 col-sm-6 col-12 ">
+                  <label className="label-control">Last Discussion :</label>
                   <textarea
                     name="Notes"
                     id="Notes"
