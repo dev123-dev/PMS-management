cf40218a610275a2f45cf110e65f48196d0495e9
dev123-dev <dev@pinnaclemedia.in>
2022-07-15 15:57:34 +0530

    pra

--- client/src/components/dashboard/JobNotes.js
+++ client/src/components/dashboard/JobNotes.js
@@ -2,7 +2,7 @@ import React, { useState, Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getSelectedClientDeatils } from "../../actions/projects";
-
+import { Link } from "react-router-dom";
 import Spinner from "../layout/Spinner";
 
 const JobNotes = ({
@@ -29,36 +29,93 @@ const JobNotes = ({
 
     isSubmitted: false,
   });
+  const [showHide, setShowHide] = useState({
+    showStandardinstructionSection: false,
+  });
+  const [showHide1, setShowHide1] = useState({
+    showProjectInstructionSection: false,
+  });
+
+  const { showStandardinstructionSection } = showHide;
+  const { showProjectInstructionSection } = showHide1;
+  const onstandardinstruction = () => {
+    setShowHide({
+      ...showHide,
+      showStandardinstructionSection: true,
+    });
+    setShowHide1({
+      ...showHide1,
+      showProjectInstructionSection: false,
+    });
+  };
+
+  const onprojectinstruction = () => {
+    setShowHide1({
+      ...showHide1,
+      showProjectInstructionSection: true,
+    });
+    setShowHide({
+      ...showHide,
+      showStandardinstructionSection: false,
+    });
+  };
 
   return !isAuthenticated || !user || !users ? (
     <Spinner />
   ) : (
     <Fragment>
+      <div className="row col-lg-12 col-md-12 col-sm-12 col-12">
+        <div className="col-lg-6 col-md-12 col-sm-12 col-12">
+          <Link
+            to="#"
+            className="btnLink"
+            onClick={() => onstandardinstruction()}
+          >
+            Standard Instruction
+          </Link>
+        </div>
+        <div className="col-lg-6 col-md-12 col-sm-12 col-12">
+          <Link
+            to="#"
+            className="btnLink"
+            onClick={() => onprojectinstruction()}
+          >
+            Project Instruction
+          </Link>
+        </div>
+      </div>
       <div className="col-lg-12 col-md-12 col-sm-12 col-12">
-        <label className="label-control">Standard Instruction :</label>
-        <textarea
-          className="textarea form-control"
-          rows="4"
-          placeholder="Standard Instruction"
-          style={{ width: "100%" }}
-          readOnly
-        >
-          {selectedClientData.standardInstruction}
-        </textarea>
+        {showStandardinstructionSection && (
+          <>
+            <label className="label-control">Standard Instruction :</label>
+            <textarea
+              className="textarea form-control"
+              rows="4"
+              placeholder="Standard Instruction"
+              style={{ width: "100%" }}
+              readOnly
+            >
+              {selectedClientData.standardInstruction}
+            </textarea>
+          </>
+        )}
       </div>
+
       <br />
-      <div className=" col-lg-12 col-md-12 col-sm-12 col-12">
-        <label className="label-control">Project Instruction :</label>
-        <textarea
-          className="textarea form-control"
-          rows="10"
-          placeholder="Project Instruction"
-          style={{ width: "100%" }}
-          readOnly
-        >
-          {allnotesdata.projectNotes}
-        </textarea>
-      </div>
+      {showProjectInstructionSection && (
+        <div className=" col-lg-12 col-md-12 col-sm-12 col-12">
+          <label className="label-control">Project Instruction :</label>
+          <textarea
+            className="textarea form-control"
+            rows="10"
+            placeholder="Project Instruction"
+            style={{ width: "100%" }}
+            readOnly
+          >
+            {allnotesdata.projectNotes}
+          </textarea>
+        </div>
+      )}
     </Fragment>
   );
 };
