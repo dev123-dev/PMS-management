95623b65bc5b82a284e9f215484b210511bb04a3
dev123-dev <dev@pinnaclemedia.in>
2022-08-12 08:57:58 +0530

    pra

--- client/src/components/dct/AllStatuschange.js
+++ client/src/components/dct/AllStatuschange.js
@@ -64,24 +64,23 @@ const AllStatuschange = ({
   };
 
   const onSubmit = (e) => {
-    e.preventDefault();
-    const finalData = {
-      callStatus: callStatus.value,
-      callDate: startStatusDate,
-      callNote: callNote,
-      callEnteredDate: todayDateymd,
-      callFromId: user._id,
-      callFromName: user.userName,
-    };
-    console.log(finalData);
-
-    setFormData({
-      ...formData,
-      callStatus: "",
-      callDate: "",
-      callNote: "",
-      isSubmitted: true,
-    });
+    // e.preventDefault();
+    // const finalData = {
+    //   callStatus: callStatus.value,
+    //   callDate: startStatusDate,
+    //   callNote: callNote,
+    //   callEnteredDate: todayDateymd,
+    //   callFromId: user._id,
+    //   callFromName: user.userName,
+    // };
+    // console.log(finalData);
+    // setFormData({
+    //   ...formData,
+    //   callStatus: "",
+    //   callDate: "",
+    //   callNote: "",
+    //   isSubmitted: true,
+    // });
   };
 
   return !isAuthenticated || !user || !users ? (
