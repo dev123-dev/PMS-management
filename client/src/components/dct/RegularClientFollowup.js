979e47ac06171e7a3460b3a39721b3be94f16384
lathesh <dev@pinnaclemedia.in>
2022-08-16 14:34:54 +0530

    pra

--- client/src/components/dct/RegularClientFollowup.js
+++ client/src/components/dct/RegularClientFollowup.js
@@ -62,70 +62,31 @@ const RegularClientFollowup = ({
             </div>
             <div className=" col-lg-2 col-md-11 col-sm-10 col-10 py-2">
               <Select
-                name="projectStatusCategory"
-                //  options={StatusCategory}
+                name="countryName"
+                //   options={allcountry}
                 isSearchable={true}
-                //  value={projectStatusCategory}
-                placeholder="Select"
-                //  onChange={(e) => onStatuscatChange(e)}
-                theme={(theme) => ({
-                  ...theme,
-                  height: 26,
-                  minHeight: 26,
-                  borderRadius: 1,
-                  colors: {
-                    ...theme.colors,
-                    primary: "black",
-                  },
-                })}
+                //   value={country}
+                placeholder="Select Region"
+                //   onChange={(e) => oncountryChange(e)}
+                required
               />
             </div>
 
             <div className=" col-lg-2 col-md-11 col-sm-10 col-10 py-2">
               <Select
-                name="projectStatusCategory"
-                //  options={StatusCategory}
+                name="companyName"
+                //  options={allclient}
                 isSearchable={true}
-                //value={projectStatusCategory}
-                placeholder="Select"
-                //  onChange={(e) => onStatuscatChange(e)}
-                theme={(theme) => ({
-                  ...theme,
-                  height: 26,
-                  minHeight: 26,
-                  borderRadius: 1,
-                  colors: {
-                    ...theme.colors,
-                    primary: "black",
-                  },
-                })}
+                //  value={clients}
+                placeholder="Select Lead"
+                //  onChange={(e) => onclientsChange(e)}
+                required
               />
             </div>
-            <div className=" col-lg-2 col-md-11 col-sm-10 col-10 py-2">
-              <Select
-                name="projectStatusCategory"
-                //  options={StatusCategory}
-                isSearchable={true}
-                //  value={projectStatusCategory}
-                placeholder="Select"
-                //   onChange={(e) => onStatuscatChange(e)}
-                theme={(theme) => ({
-                  ...theme,
-                  height: 26,
-                  minHeight: 26,
-                  borderRadius: 1,
-                  colors: {
-                    ...theme.colors,
-                    primary: "black",
-                  },
-                })}
-              />
-            </div>
-
-            <div className="col-lg-4 col-md-11 col-sm-12 col-11 py-3">
+            <div className="col-lg-6 col-md-11 col-sm-12 col-11 py-3">
               <button
                 className="btn btn_green_bg float-right"
-                // onClick={() => onClickReset()}
+                //  onClick={() => onClickReset()}
               >
                 Refresh
               </button>
@@ -195,13 +156,19 @@ const RegularClientFollowup = ({
                 </div>
               </div>
               <div className=" col-lg-12 col-md-6 col-sm-6 col-12 card-new  no_padding ">
-                <div className="col-lg-12 col-md-12 col-sm-12 col-12 no_padding ">
+                <div
+                  className="col-lg-12 col-md-12 col-sm-12 col-12 no_padding "
+                  style={{ height: "30vh" }}
+                >
                   <label className="sidePartHeading ">Status</label>
                   <AllStatuschange />
                 </div>
               </div>
               <div className=" col-lg-12 col-md-6 col-sm-6 col-12 card-new no_padding sidePartHeight">
-                <div className="col-lg-12 col-md-12 col-sm-12 col-12 no_padding ">
+                <div
+                  className="col-lg-12 col-md-12 col-sm-12 col-12 no_padding "
+                  style={{ height: "23vh" }}
+                >
                   <label className="sidePartHeading ">
                     Last Message Details
                   </label>
