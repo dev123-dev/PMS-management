e1450dccf1c6cff9edaa68c654d7471e489d9de1
lathesh <dev@pinnaclemedia.in>
2022-08-19 15:59:42 +0530

    pra

--- client/src/components/dct/AllDctClients.js
+++ client/src/components/dct/AllDctClients.js
@@ -132,7 +132,7 @@ const AllDctClients = ({
                                       },
                                     }}
                                   >
-                                    Add client
+                                    Edit
                                   </Link>
                                 </>
                               </td>
