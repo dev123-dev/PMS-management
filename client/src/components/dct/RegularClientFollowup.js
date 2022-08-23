7b714d0db6cfb44dbf7628519ac3f3434598d155
lathesh <dev@pinnaclemedia.in>
2022-08-23 12:25:24 +0530

    pra

--- client/src/components/dct/RegularClientFollowup.js
+++ client/src/components/dct/RegularClientFollowup.js
@@ -117,9 +117,41 @@ const RegularClientFollowup = ({
     });
   };
 
+  const allemp = [{ empId: null, label: "All", value: null }];
+  marketingEmployees.map((emp) =>
+    allemp.push({
+      empId: emp._id,
+      label: emp.empFullName,
+      value: emp.empFullName,
+    })
+  );
+
+  const [emp, getempData] = useState();
+  const [empId, setempID] = useState();
+  const onempChange = (e) => {
+    getempData(e);
+    setempID(e.empId);
+    getDctClientDetails({
+      countryId: countryId,
+      clientsId: clients ? clients.clientsId : null,
+      assignedTo: e.empId,
+    });
+    getDctClientDetailsDD({
+      countryId: countryId,
+      clientsId: clients ? clients.clientsId : null,
+      assignedTo: e.empId,
+    });
+    setFilterData({
+      countryId: countryId,
+      clientsId: clients ? clients.clientsId : null,
+      assignedTo: e.empId,
+    });
+  };
+
   const onClickReset = () => {
     getcountryData("");
     getclientsData("");
+    getempData("");
     getDctClientDetails({ dctClientCategory: "RC" });
     getDctClientDetailsDD({ dctClientCategory: "RC" });
     setFilterData({ dctClientCategory: "RC" });
@@ -158,7 +190,22 @@ const RegularClientFollowup = ({
                 required
               />
             </div>
-            <div className="col-lg-6 col-md-11 col-sm-12 col-11 py-3">
+            {(user.userGroupName && user.userGroupName === "Administrator") ||
+            user.userGroupName === "Super Admin" ? (
+              <div className=" col-lg-2 col-md-11 col-sm-10 col-10 py-2">
+                <Select
+                  name="empFullName"
+                  options={allemp}
+                  isSearchable={true}
+                  value={emp}
+                  placeholder="Select Emp"
+                  onChange={(e) => onempChange(e)}
+                />
+              </div>
+            ) : (
+              <></>
+            )}
+            <div className="col-lg-4 col-md-11 col-sm-12 col-11 py-3">
               <button
                 className="btn btn_green_bg float-right"
                 onClick={() => onClickReset()}
