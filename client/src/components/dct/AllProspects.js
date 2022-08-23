22cb2088576c29943621a407c48974a02ba84d83
lathesh <dev@pinnaclemedia.in>
2022-08-23 12:28:03 +0530

    pra

--- client/src/components/dct/AllProspects.js
+++ client/src/components/dct/AllProspects.js
@@ -199,7 +199,7 @@ const AllProspects = ({
       <div className="container container_align ">
         <section className="sub_reg">
           <div className="row col-lg-12 col-md-12 col-sm-12 col-12 no_padding">
-            <div className=" col-lg-1 col-md-11 col-sm-10 col-10">
+            <div className=" col-lg-2 col-md-11 col-sm-10 col-10">
               <h5 className="heading_color">All Prospects</h5>
             </div>
             <div className=" col-lg-2 col-md-11 col-sm-10 col-10 py-2">
@@ -241,7 +241,7 @@ const AllProspects = ({
               <></>
             )}
 
-            <div className="col-lg-5 col-md-11 col-sm-12 col-11 py-3">
+            <div className="col-lg-4 col-md-11 col-sm-12 col-11 py-3">
               <button
                 className="btn btn_green_bg float-right"
                 onClick={() => onClickReset()}
