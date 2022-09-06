b357883f5af69bfbde6931df44a809fd8373aeaa
lathesh <dev@pinnaclemedia.in>
2022-09-06 15:51:31 +0530

    pra

--- client/src/components/sct/AllDemos.js
+++ client/src/components/sct/AllDemos.js
@@ -110,7 +110,7 @@ const AllDemos = ({
                 //onChange={(e) => onstaffStateChange(e)}
               />
             </div>
-            <div className="col-lg-3 col-md-11 col-sm-12 col-11 py-3">
+            <div className="col-lg-5 col-md-11 col-sm-12 col-11 py-3">
               <button
                 className="btn btn_green_bg float-right"
                 onClick={() => onClickReset()}
