268fa92f721e691b357e4a82d813ad41bc50cfca
dev123-dev <dev@pinnaclemedia.in>
2022-09-22 12:17:19 +0530

    PRQA

--- client/src/components/sct/SctQuotationpdfprint.js
+++ client/src/components/sct/SctQuotationpdfprint.js
@@ -134,51 +134,53 @@ const SctQuotationpdfprint = ({
   return !isAuthenticated || !user || !users ? (
     <Spinner />
   ) : (
-    <PDFViewer style={styles.viewer}>
-      {/* Start of the document*/}
+    <>
+      <div className="" style={{ marginTop: "54px" }}></div>
+      <PDFViewer style={styles.viewer}>
+        {/* Start of the document*/}
 
-      <Document>
-        {/*render a single page*/}
-        <Page size="A4" style={styles.page}>
-          <View style={styles.section}>
-            <Text>Quotation</Text>
-          </View>
-          <View style={styles.section}>
-            <Text>Quotation No #:{quotationNo}</Text>
-            <Text>Quotation Date:{startquotationDate}</Text>
-          </View>
-
-          <View style={(styles.table, styles.section)}>
-            <View style={[styles.row]}>
-              <Text
-                style={{
-                  // border: "1 px solid black",
-                  width: "600px",
-                  height: "100%",
-                }}
-              >
-                From: {"\n"}
-                {companyName}
-                {"\n"}
-                {companyAddress}
-              </Text>
-
-              <Text
-                style={{
-                  paddingLeft: "10px",
-                  // border: "1 px solid black",
-                  width: "600px",
-                  height: "100%",
-                }}
-              >
-                To:{"\n"}
-                {forName} {"\n"}
-                {forAddress}
-              </Text>
+        <Document>
+          {/*render a single page*/}
+          <Page size="A4" style={styles.page}>
+            <View style={styles.section}>
+              <Text>Quotation</Text>
             </View>
-          </View>
+            <View style={styles.section}>
+              <Text>Quotation No #:{quotationNo}</Text>
+              <Text>Quotation Date:{startquotationDate}</Text>
+            </View>
 
-          {/* <View style={(styles.table, styles.section)}>
+            <View style={(styles.table, styles.section)}>
+              <View style={[styles.row]}>
+                <Text
+                  style={{
+                    // border: "1 px solid black",
+                    width: "600px",
+                    height: "100%",
+                  }}
+                >
+                  From: {"\n"}
+                  {companyName}
+                  {"\n"}
+                  {companyAddress}
+                </Text>
+
+                <Text
+                  style={{
+                    paddingLeft: "10px",
+                    // border: "1 px solid black",
+                    width: "600px",
+                    height: "100%",
+                  }}
+                >
+                  To:{"\n"}
+                  {forName} {"\n"}
+                  {forAddress}
+                </Text>
+              </View>
+            </View>
+
+            {/* <View style={(styles.table, styles.section)}>
             <View style={[styles.row]}>
               <Text
                 style={{
@@ -197,73 +199,74 @@ const SctQuotationpdfprint = ({
             </View>
           </View> */}
 
-          <View style={(styles.table, styles.section)}>
-            <View style={[styles.row, styles.bold, styles.header]}>
-              <Text style={styles.row1}>Item</Text>
-              <Text style={styles.row2}>GST </Text>
-              <Text style={styles.row3}>Qty</Text>
-              <Text style={styles.row4}>Rate</Text>
-              <Text style={styles.row5}>Amount</Text>
-              <Text style={styles.row6}>CGST</Text>
-              <Text style={styles.row7}>SGST</Text>
-              <Text style={styles.row8}>Total</Text>
-              <Text style={styles.row8}>Discount</Text>
-              <Text style={styles.row8}>Grand Total</Text>
+            <View style={(styles.table, styles.section)}>
+              <View style={[styles.row, styles.bold, styles.header]}>
+                <Text style={styles.row1}>Item</Text>
+                <Text style={styles.row2}>GST </Text>
+                <Text style={styles.row3}>Qty</Text>
+                <Text style={styles.row4}>Rate</Text>
+                <Text style={styles.row5}>Amount</Text>
+                <Text style={styles.row6}>CGST</Text>
+                <Text style={styles.row7}>SGST</Text>
+                <Text style={styles.row8}>Total</Text>
+                <Text style={styles.row8}>Discount</Text>
+                <Text style={styles.row8}>Grand Total</Text>
+              </View>
+              {quotationDataLS &&
+                quotationDataLS.item.map((row, i) => (
+                  <View key={i} style={styles.row} wrap={false}>
+                    <Text style={styles.row1}>{row.itemName}</Text>
+                    <Text style={styles.row2}>{row.GST}</Text>
+                    <Text style={styles.row3}>{row.qty}</Text>
+                    <Text style={styles.row4}>{row.rate}</Text>
+                    <Text style={styles.row5}>{row.Amount}</Text>
+                    <Text style={styles.row6}>{row.CGST}</Text>
+                    <Text style={styles.row6}>{row.SGST}</Text>
+                    <Text style={styles.row6}>{row.totalAmt}</Text>
+                    <Text style={styles.row6}>{row.discount}</Text>
+                    <Text style={styles.row6}>{row.grandTotal}</Text>
+                  </View>
+                ))}
             </View>
-            {quotationDataLS &&
-              quotationDataLS.item.map((row, i) => (
-                <View key={i} style={styles.row} wrap={false}>
-                  <Text style={styles.row1}>{row.itemName}</Text>
-                  <Text style={styles.row2}>{row.GST}</Text>
-                  <Text style={styles.row3}>{row.qty}</Text>
-                  <Text style={styles.row4}>{row.rate}</Text>
-                  <Text style={styles.row5}>{row.Amount}</Text>
-                  <Text style={styles.row6}>{row.CGST}</Text>
-                  <Text style={styles.row6}>{row.SGST}</Text>
-                  <Text style={styles.row6}>{row.totalAmt}</Text>
-                  <Text style={styles.row6}>{row.discount}</Text>
-                  <Text style={styles.row6}>{row.grandTotal}</Text>
-                </View>
-              ))}
-          </View>
 
-          <View style={styles.section}>
-            <Text style={{ textAlign: "right" }}>Sub Total: {totSubTot}</Text>
-            <Text style={{ textAlign: "right" }}>
-              Amount Chargeable (in words) &nbsp;: &nbsp;
-              {toWords.convert(totSubTot, { currency: true })}
-            </Text>
-          </View>
+            <View style={styles.section}>
+              <Text style={{ textAlign: "right" }}>Sub Total: {totSubTot}</Text>
+              <Text style={{ textAlign: "right" }}>
+                Amount Chargeable (in words) &nbsp;: &nbsp;
+                {toWords.convert(totSubTot, { currency: true })}
+              </Text>
+            </View>
 
-          <View
-            style={{
-              position: "absolute",
-              bottom: "20",
-              left: "0",
-              padding: "10",
-            }}
-          >
-            <Text>Terms and Condition: </Text>
-            <Text> 1. Applicable taxes will be extra.</Text>
-            <Text> 2. Work will resume after full payment.</Text>
-          </View>
+            <View
+              style={{
+                position: "absolute",
+                bottom: "20",
+                left: "0",
+                padding: "10",
+              }}
+            >
+              <Text>Terms and Condition: </Text>
+              <Text> 1. Applicable taxes will be extra.</Text>
+              <Text> 2. Work will resume after full payment.</Text>
+            </View>
 
-          <View
-            style={{
-              position: "absolute",
-              bottom: "0",
-              left: "0",
-              padding: "10",
-            }}
-          >
-            <Text>
-              For any enquiry, reach out via email at joel@pinnaclemedia.in,
-              call on +91 99162 13542{" "}
-            </Text>
-          </View>
-        </Page>
-      </Document>
-    </PDFViewer>
+            <View
+              style={{
+                position: "absolute",
+                bottom: "0",
+                left: "0",
+                padding: "10",
+              }}
+            >
+              <Text>
+                For any enquiry, reach out via email at joel@pinnaclemedia.in,
+                call on +91 99162 13542{" "}
+              </Text>
+            </View>
+          </Page>
+        </Document>
+      </PDFViewer>
+    </>
   );
 };
 
