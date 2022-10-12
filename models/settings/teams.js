5d14cb1f2dff65e44d1d2f98f34710b71c490b55
dev123-dev <dev@pinnaclemedia.in>
2022-10-12 12:41:52 +0530

    abhi

---
+++ models/settings/teams.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+ObjectId = mongoose.Schema.ObjectId;
+
+const TeamsSchema = new mongoose.Schema({
+  teamName: {
+    type: String,
+  },
+  teamDescription: {
+    type: String,
+  },
+  teamEnteredById: {
+    type: ObjectId,
+  },
+  teamEnteredByName: {
+    type: String,
+  },
+  teamEnteredDateTime: {
+    type: String,
+  },
+  teamEditedById: {
+    type: ObjectId,
+  },
+  teamEditedByName: {
+    type: String,
+  },
+  teamEditedDateTime: {
+    type: String,
+  },
+  teamDeactiveById: {
+    type: ObjectId,
+  },
+  teamDeactiveByName: {
+    type: String,
+  },
+  teamDeactiveDateTime: {
+    type: String,
+  },
+  teamDeactiveReason: {
+    type: String,
+  },
+});
+
+module.exports = teams = mongoose.model("teams", TeamsSchema);
