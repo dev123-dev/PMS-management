bf7392c8b2b65c246175e4e0251b414a8d5a48fe
dev123-dev <dev@pinnaclemedia.in>
2023-05-06 10:52:10 +0530

    fy report and client report

--- client/src/components/dct/clientFYReport.js
+++ client/src/components/dct/clientFYReport.js
@@ -4,7 +4,6 @@ import { connect } from "react-redux";
 import { Modal } from "react-bootstrap";
 import Select from "react-select";
 import Spinner from "../layout/Spinner";
-import { Link } from "react-router-dom";
 
 import {
   getverificationProjectDeatils,
@@ -12,12 +11,11 @@ import {
   getAllFolder,
   getSelectedClientfolderDeatils,
 } from "../../actions/projects";
-import { getYear, getFYclient } from "../../actions/sct";
 import { getVerificationFolder } from "../../actions/client";
 
 import { getAllchanges, getUpdatedProjectStaus } from "../../actions/projects";
 import { w3cwebsocket } from "websocket";
-import { propTypes } from "react-bootstrap/esm/Image";
+
 //client in websocket
 //SLAP IP
 const client = new w3cwebsocket("ws://192.168.6.38:8000");
@@ -26,21 +24,15 @@ const ClientReportDetails = ({
   auth: { isAuthenticated, user, users },
   project: { allFolderName },
   client: { activeVerfificationFolders },
-  sct: { FYclient },
 
   getverificationProjectDeatils,
   getAllProjectStatusVerification,
   getUpdatedProjectStaus,
-  getYear,
-  getFYclient,
+
   getVerificationFolder,
   getSelectedClientfolderDeatils,
 }) => {
-  let financialyear = JSON.parse(localStorage.getItem("financialYear"));
-
-  useEffect(() => {
-    getYear();
-  }, []);
+  console.log("allFolderName", allFolderName);
   useEffect(() => {
     client.onopen = () => {
       console.log("webSocket client connected");
@@ -53,32 +45,26 @@ const ClientReportDetails = ({
   useEffect(() => {
     getAllFolder();
   }, [getAllFolder]);
-
-  const [clientData, setClientData1] = useState("");
-  const [Year, setYear] = useState("");
+  useEffect(() => {
+    getverificationProjectDeatils();
+  }, [getverificationProjectDeatils]);
+  useEffect(() => {
+    getAllProjectStatusVerification();
+  }, [getAllProjectStatusVerification]);
+  useEffect(() => {
+    getVerificationFolder();
+  }, [getVerificationFolder]);
+  // useEffect(() => {
+  //   getVerificationClients();
+  // }, [getVerificationClients]);
+  useEffect(() => {
+    getSelectedClientfolderDeatils({
+      clientFolderName: allFolderName && allFolderName.clientFolderName,
+    });
+  }, [getSelectedClientfolderDeatils]);
 
   const onfolderClientChange = (e) => {
     setClientData1(e);
-
-    let folderData = {
-      folder: e.value,
-    };
-    console.log("folderData", folderData);
-    getFYclient(folderData);
-  };
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setendDate] = useState("");
-
-  const onYearChange = (e) => {
-    setYear(e);
-    setStartDate(e.value.startDate);
-    setendDate(e.value.endDate);
-    let selYear = {
-      startDate: e.value.startDate,
-      endDate: e.value.endDate,
-    };
-    getFYclient(selYear);
-
     // let selDateData = {
     //   folder: e.value,
     //   statusId: projectStatusData.value,
@@ -86,24 +72,11 @@ const ClientReportDetails = ({
     // };
   };
 
-  const year = [];
-  financialyear &&
-    financialyear.map((ele) =>
-      year.push({
-        value: {
-          startDate: ele.startDate,
-          endDate: ele.endDate,
-        },
-
-        label: ele._id,
-      })
-    );
-
   const onClickReset = () => {
-    setYear("");
-
     setClientData1("");
   };
+
+  const [clientData, setClientData1] = useState("");
 
   const activeClientsOpt = [];
   allFolderName &&
@@ -113,19 +86,6 @@ const ClientReportDetails = ({
         value: clientsData._id,
       })
     );
-
-  const handleGoToAllMember = (client) => {
-    const finalData = {
-      Id: client._id,
-      clientName: client.clientName,
-      clientFolderName: client.clientFolderName,
-      startDate: startDate,
-      endDate: endDate,
-    };
-    console.log("finalData", finalData);
-    // getShgMembers(finalData);
-    // setSubmitted(true);
-  };
   return !isAuthenticated || !user || !users ? (
     <Spinner />
   ) : (
@@ -133,32 +93,11 @@ const ClientReportDetails = ({
       <div className="container container_align ">
         <section className="sub_reg">
           <div className="row col-lg-12 col-md-12 col-sm-12 col-12 no_padding">
-            <div className=" col-lg-2 col-md-11 col-sm-10 col-10">
-              <h5 className="heading_color">Client Report Details</h5>
+            <div className=" col-lg-12 col-md-11 col-sm-10 col-10">
+              <h5 className="heading_color">Client FY Report</h5>
             </div>
 
-            <div className="col-lg-2 col-md-11 col-sm-10 col-10 py-2">
-              <Select
-                name="projectStatusData"
-                options={year}
-                value={Year}
-                isSearchable={true}
-                placeholder="Select"
-                onChange={(e) => onYearChange(e)}
-              />
-            </div>
-            <div className="col-lg-2 col-md-11 col-sm-10 col-10 py-2">
-              <Select
-                name="clientData"
-                isSearchable={true}
-                value={clientData}
-                options={activeClientsOpt}
-                placeholder="Select Folder"
-                onChange={(e) => onfolderClientChange(e)}
-              />
-            </div>
-
-            <div className="col-lg-4 col-md-11 col-sm-12 col-11 py-3">
+            <div className="col-lg-12 col-md-11 col-sm-12 col-11 ">
               <button
                 className="btn btn_green_bg float-right"
                 onClick={() => onClickReset()}
@@ -179,40 +118,18 @@ const ClientReportDetails = ({
                     <thead>
                       <tr>
                         <th>Sl no</th>
-                        <th>Client Name</th>
-                        <th>Client Type</th>
+                        <th>Month</th>
+
                         <th>Project Name</th>
-                        <th>Project date</th>
+
                         <th>Project QTY</th>
                       </tr>
                     </thead>
                     <tbody>
-                      {FYclient &&
-                        FYclient.map((client, idx) => {
-                          var projectdate = "";
-                          if (client.projectDate) {
-                            var ED1 = client.projectDate.split(/\D/g);
-                            projectdate = [ED1[2], ED1[1], ED1[0]].join("-");
-                          }
-                          return (
-                            <tr key={idx}>
-                              <td>{idx + 1}</td>
-                              <td>
-                                <Link
-                                  to="/client-FY-report"
-                                  className="btnLink"
-                                  onClick={() => handleGoToAllMember(client)}
-                                >
-                                  {client.clientName}
-                                </Link>
-                              </td>
-                              <td>{client.clientTypeVal}</td>
-                              <td>{client.projectName}</td>
-                              <td>{projectdate}</td>
-                              <td>{client.projectQuantity}</td>
-                            </tr>
-                          );
-                        })}
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
                     </tbody>
                   </table>
                 </div>
@@ -247,7 +164,6 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
   project: state.project,
   client: state.client,
-  sct: state.sct,
 });
 
 export default connect(mapStateToProps, {
@@ -257,7 +173,6 @@ export default connect(mapStateToProps, {
   getUpdatedProjectStaus,
   getAllFolder,
   getSelectedClientfolderDeatils,
-  getYear,
-  getFYclient,
+
   getVerificationFolder,
 })(ClientReportDetails);
